627011780c9c46b19e9fe56c6e75232e10ecbb6a
Ricardo Sanchez <me@richnologies.io>
2020-02-13 11:11:30 +0100

    minor change

--- src/language-js/printer-estree.js
+++ src/language-js/printer-estree.js
@@ -5130,7 +5130,7 @@ function printMemberChain(path, options, print) {
   // If the last group is a function it's okay to inline if it fits.
   if (
     hasComment ||
-    callExpressions.length >= 6 ||
+    callExpressions.length >= 10 ||
     printedGroups.slice(0, -1).some(willBreak) ||
     /**
      *     scopes.filter(scope => scope.value !== '').map((scope, i) => {
